bf1e5a5b612bdf6571243299e1d40803ecd64dfa
Volodymyr Komar <v.komar@softonix.org>
2026-03-02 19:54:05 +0100

    Fix: Solution

--- tests/profiles/read/readUserProfileWithEmptyAuthHeader.spec.js
+++ tests/profiles/read/readUserProfileWithEmptyAuthHeader.spec.js
@@ -4,9 +4,7 @@ import { DEFAULT_IMAGE_LINK } from '../../../src/constants/defaultValues';
 test('Read user profile with empty auth header', async ({
   profilesApi,
   registeredUser,
-}) => {
-  registeredUser['token'] = '';
-  
+}) => {  
   const response = await profilesApi.getProfile(registeredUser.username);
 
   await profilesApi.assertSuccessResponseCode(response);
--- tests/users/update/updateUserThatNotExist.spec.js
+++ tests/users/update/updateUserThatNotExist.spec.js
@@ -1,15 +1,12 @@
 import { test } from '../../_fixtures/fixtures';
-import { INVALID_EMAIL_OR_PASSWORD_MESSAGE } from '../../../src/constants/authErrorMessages';
 
 test('Update user that does not exist', async ({
   usersApi,
   registeredUser,
 }) => {
-  const response = await usersApi.updateUser({
-    email: 'notexisting@email.com',
-    password: registeredUser.password,
-    username: registeredUser.username,
-  });
+  registeredUser['token'] = '';
+
+  const response = await usersApi.updateUser(registeredUser);
 
   await usersApi.assertUnauthorizedResponseCode(response);
 });
